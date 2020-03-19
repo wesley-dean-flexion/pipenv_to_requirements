58d6c6ca2aaa2c0c89b020b9655214878f863f69
Wes Dean <wdean@flexion.us>
2020-03-19 11:54:46 -0400

    Update comments

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,37 @@
 FROM python:3.7-slim
 
+##
+## Parameterize the build
+##
+
+# where to use as a data source (largely unneeded in most cases)
 ARG WORKING_DIRECTORY=/data
+
+# the unprivileged system username to create
 ARG username=ptor
+
+# the unprivileged system group to create
 ARG groupname=ptor
 
+# make sure the working directory, the user, and its group all exist and are
+# owned properly
 RUN mkdir -p ${WORKING_DIRECTORY} \
   && groupadd --system ${groupname} \
   && useradd --system --create-home --gid ${groupname} ${username} \
   && chown --recursive ${username}:${groupname} ${WORKING_DIRECTORY}
 
+# move to the working directory
 WORKDIR ${WORKING_DIRECTORY}
 
-RUN pip install pipenv_to_requirements 
+# make sure we have the Python tools needed to do the conversion
+RUN pip install pipenv_to_requirements
 
+# install the entrypoint script and make it executable
 COPY entrypoint.sh /bin/entrypoint.sh
 RUN chmod 755 /bin/entrypoint.sh
 
+# switch to the unprivileged system user created previously
 USER ${username}
 
+# set the default entrypoint -- the command to run -- to the script we installed
 ENTRYPOINT ["/bin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,64 @@
 #!/bin/sh
 
-output_filename="/tmp/requirements.txt"
+##
+## defaults
+##
+
+# where to write (and then read) the Pipfile from STDIN
 input_filename="/tmp/Pipfile"
+
+# where to write (and then read) the Pipfile.lock from STDIN when using -l
 input_lock_filename="/tmp/Pipfile.lock"
 
+# where to write the output to be passed out via STDOUT
+output_filename="/tmp/requirements.txt"
+
+# quiet (no logs) flag for pipenv_to_requirements
 quiet=""
+
+# freeze versions flag for pipenv_to_requirements
 freeze=""
+
+# what to write and where
 output="--output $output_filename"
+
+# if "true", then a Pipfile.lock was passed via STDIN; when "false", Pipfile
 lockfile_provided="false"
 
+##
+## Parse CLI arguments
+##
+
 while getopts "odfql" option; do
-  case $option in
+  case "$option" in
     o ) output="--output $output_filename" ;;
     d ) output="--dev-output $output_filename " ;;
     f ) freeze="--freeze" ;;
     q ) quiet="--quiet" ;;
     l ) lockfile_provided="true" ;;
+    * ) echo "Invalid option: ${option}; quitting."; exit 1 ;;
   esac
 done
 
+# just in case we get strings (e.g., filenames) passed after any flags are
+# parsed by getopts are caught and put into $1, $2, etc..
 shift $((OPTIND - 1))
 
-cd $(dirname "$input_filename")
+# make sure we're in the right directory
+cd "$(dirname "$input_filename")" || exit 1
 
+# if we get a Pipfile.lock, no need to run `pipenv lock` on it, so skip it
 if [ "$lockfile_provided" = "true" ] ; then
+  # write directly to Pipfile.lock
   cat > "$input_lock_filename"
 else
   cat > "$input_filename"
+  # parse Pipfile -> Pipfile.lock
   pipenv --bare lock > /dev/null 2>&1
 fi
 
+# convert the Pipfile.lock to requirements.txt (or requirements-dev.txt)
 pipenv_to_requirements $quiet $freeze $output
 
-cat $output_filename
+# write the results to STDOUT
+cat "$output_filename"
